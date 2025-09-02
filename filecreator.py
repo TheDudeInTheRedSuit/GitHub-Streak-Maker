59e838a5a1600c11ba5102e779c104f5f7f27503
TheDudeInTheRedSuit <kylelsturrock@gmail.com>
2025-09-02 20:28:55 +0200

    made a streak hehe
    made a streak hehe

--- filecreator.py
+++ filecreator.py
@@ -2,7 +2,7 @@ def makefile(text, nooflines):
   nooflines = int(nooflines)
   with open("streakmaker.txt", "w") as f:
       for i in range(nooflines):
-        f.write("text\n")
+        f.write(f"{text}\n")
 
 lines = input("how many lines?")
 text = input("what text do you want placed")
