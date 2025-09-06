ea2ee71a60f8c72c2b038167f4dc88acd247bf5f
DJ_Theron <97227143+DJTheron@users.noreply.github.com>
2025-09-06 18:54:19 +0200

    Update streak-automator.sh

--- streak-automator.sh
+++ streak-automator.sh
@@ -1,2 +1,7 @@
-
+cd ~
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg && sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null && sudo apt update && sudo apt install gh -y
+gh --version
+gh auth login
+gh repo create my-streak --public --clone
+cd my-streak
 # add commands that can create a repo and then run the python and then commit, run the python, commit tbh idk if this is allowed per GitHub guidelines idk
